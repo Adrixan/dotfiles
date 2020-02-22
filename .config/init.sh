368f131df8fd15b6b8a7011e6beed2d1de5b18f3
Peter Aufner <peter@code-alongsi.de>
2020-02-22 18:38:06 +0100

    improved init.sh script

--- .config/init.sh
+++ .config/init.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-ln -s ~/.config/zsh/themes/powerlevel9k .oh-my-zsh/custom/themes/powerlevel9k ;
+ln -s ~/.config/zsh/themes/powerlevel9k ~/.oh-my-zsh/custom/themes/powerlevel9k ;
 offlineimap
 vdirsyncer discover
 vidrsyncer sync
+ln -s ~/.config/oh-my-zsh-modules/autoupdate ~/.oh-my-zsh/plugins/autoupdate
+ln -s ~/.config/oh-my-zsh-modules/you-should-use ~/.oh-my-zsh/plugins/you-should-use
