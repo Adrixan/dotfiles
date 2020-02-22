c0046536aec207a0dc718e9b05bdb515c0289926
Peter Aufner <peter@code-alongsi.de>
2020-02-22 18:40:27 +0100

    Fixed typo

--- .config/init.sh
+++ .config/init.sh
@@ -2,6 +2,6 @@
 ln -s ~/.config/zsh/themes/powerlevel9k ~/.oh-my-zsh/custom/themes/powerlevel9k ;
 offlineimap
 vdirsyncer discover
-vidrsyncer sync
+vdirsyncer sync
 ln -s ~/.config/oh-my-zsh-modules/autoupdate ~/.oh-my-zsh/plugins/autoupdate
 ln -s ~/.config/oh-my-zsh-modules/you-should-use ~/.oh-my-zsh/plugins/you-should-use
