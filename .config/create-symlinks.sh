f471580c51f11edd8439a559cb4a68e2c71caebf
Peter Aufner <peter@code-alongsi.de>
2020-03-20 17:18:59 +0100

    Moved to new layout and added symlink for oh-my-zsh theme

--- .config/create-symlinks.sh
+++ .config/create-symlinks.sh
@@ -2,3 +2,4 @@
 
 ln -s ~/.config/oh-my-zsh-modules/you-should-use ~/.oh-my-zsh/plugins/you-should-use
 ln -s ~/.config/oh-my-zsh-modules/autoupdate ~/.oh-my-zsh/plugins/autoupdate
+ln -s ~/.config/zsh/themes/powerlevel9k ~/.oh-my-zsh/themes/
