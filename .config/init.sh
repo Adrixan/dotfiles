335d36164d44b8d1237f5956aa572bafdfcef4a6
Peter Aufner <peter@code-alongsi.de>
2020-07-01 13:19:39 +0200

    Fix vim setup

--- .config/init.sh
+++ .config/init.sh
@@ -5,3 +5,5 @@ vdirsyncer discover
 vdirsyncer sync
 ln -s ~/.config/oh-my-zsh-modules/autoupdate ~/.oh-my-zsh/plugins/autoupdate
 ln -s ~/.config/oh-my-zsh-modules/you-should-use ~/.oh-my-zsh/plugins/you-should-use
+nvim +PluginInstall +qall ;
+nvim +UpdateRemotePlugins ;
