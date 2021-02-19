5a6eee345ab1493c77f430e3b5dbbc12a0031b2b
Peter Aufner <peter@code-alongsi.de>
2021-02-19 20:11:52 +0100

    Add support for tpm

--- .config/init.sh
+++ .config/init.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 ln -s ~/.config/zsh/themes/powerlevel9k ~/.oh-my-zsh/custom/themes/powerlevel9k ;
-offlineimap
-vdirsyncer discover
-vdirsyncer sync
+#offlineimap
+#vdirsyncer discover
+#vdirsyncer sync
 ln -s ~/.config/oh-my-zsh-modules/autoupdate ~/.oh-my-zsh/plugins/autoupdate
 ln -s ~/.config/oh-my-zsh-modules/you-should-use ~/.oh-my-zsh/plugins/you-should-use
 nvim +PluginInstall +qall ;
