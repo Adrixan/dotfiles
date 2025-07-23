21dee7fdaf1bc143a81aaaf0393e6f6938ba7d83
Peter Aufner <peter@code-alongsi.de>
2025-07-23 15:02:33 +0200

    Remove theming and plugins

--- .config/create-symlinks.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-ln -s ~/.config/oh-my-zsh-modules/you-should-use ~/.oh-my-zsh/plugins/you-should-use
-ln -s ~/.config/oh-my-zsh-modules/autoupdate ~/.oh-my-zsh/plugins/autoupdate
-# ln -s ~/.config/zsh/themes/powerlevel9k ~/.oh-my-zsh/themes/
-ln -s ~/.addon-repos/dracula-zsh/dracula.zsh-theme ~/.oh-my-zsh/themes/dracula.zsh-theme
-ln -s ~/.addon-repos/dracula-zsh/lib ~/.oh-my-zsh/themes/lib
-
